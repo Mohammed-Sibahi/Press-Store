c140879974871df863d38cd32a1d25c7116b2679
Mohammed Sibahi <sibahi@live.com>
2021-08-06 00:35:05 +0300

    build & style the admin section

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer>
+     <div class="row">
+       <ul class="footer">
+         <li class="footer-item">About</li>
+         <li>|</li>
+         <li class="footer-item">Contact us</li>
+         <li>|</li>
+         <li class="footer-item">Privacy notice</li>
+         <li>|</li>
+         <li class="footer-item">Do Not Sell My Personal Information</li>
+         <li>|</li>
+         <li class="footer-item">Terms of use</li>
+        </ul>
+        <p style="margin-left: 250px;">© 2021 Pearson Education. All rights reserved.</p>
+     </div>
+   </footer> 
+
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+      integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
+      integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT"
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script>
+      $(document).ready(function () {
+        $("#wrapper").click(function () {
+          $(".wrapper").toggleClass("wrapper-2");
+        });
+      });
+    </script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,116 @@
+
+    <?php include 'header.php';?>
+    
+    <!--Body-->
+    <div id="wrapper" class="wrapper">
+      <div class="flex-caption block-transparent-black">
+        <img
+          class="image-1"
+          src="https://ptgmedia.pearsoncmg.com/imprint_downloads/microsoftpress/home/slider_newsletter_mar2021_918x238.jpg"
+        />
+        <ul class="subheader col-lg-3">
+          <li><h3 class="">Newsletter</h3></li>
+          <li>
+            <p class="">
+              Be the first to hear about new Microsoft Press titles, special
+              offers, events, and more.
+            </p>
+          </li>
+          <li>
+            <strong
+              ><a style="text-decoration: underline; color: black" href="#"
+                >Sign up Now.</a
+              ></strong
+            >
+          </li>
+        </ul>
+      </div>
+
+      <!--Title-->
+
+      <h3 style="margin-left: 60px">Shop topics</h3>
+      <ul class="topics">
+        <li>Windows</li>
+        <li>Microsoft Office</li>
+        <li>Servers</li>
+        <li>Programming</li>
+        <li>Certification & Training</li>
+      </ul>
+      <div class="row">
+        <div class="category-1 col-lg-4">
+
+          <ul class="nav-4">
+            <li class="li-1">
+              <button type="button" class="btn-1">New arrivals</button>
+            </li>
+            <li class="li-2">Popular</li>
+          </ul>
+          <ul class="book-list">
+            <li><img style="width: 120px;" src="https://images-na.ssl-images-amazon.com/images/I/415rt3iphpS._SX258_BO1,204,203,200_.jpg" /></li>
+            <li><img style="width: 120px;" src="https://tutsnode.com/wp-content/uploads/2021/06/Introduction-to-Microsoft-Power-BI.jpg" /></li>
+            <li><img style="width: 120px;" src="https://www.microsoftpressstore.com/ShowCover.aspx?isbn=9780137343423" /></li>
+            <li><img style="width: 120px;" src="https://www.microsoftpressstore.com/ShowCover.aspx?isbn=9780137489497" /></li>
+
+          </ul>
+        </div>
+          
+      </div>
+
+      <div style="margin-left: 60px;" class="category-2">
+         <h5>eBook Deal of the Week</h5>  
+         
+         <div class="card mb-3" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="https://www.microsoftpressstore.com/ShowCover.aspx?isbn=9780136485452" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Microsoft Azure Sentinel</h5>
+                <p class="card-text">Sale Price: $15.99</p>
+                <p class="card-text">You save: 50%</p>
+                <button type="button" class="btn btn-primary">Learn More</button>
+                
+                <button  type="button" class="btn btn-success">Buy</button>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> <!--figure out how to activate it in PHP-->
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="row last-list">
+        <div class="discover col-lg-3 ">
+          <h5 style="margin-left: 30px;">Discover</h5>
+          <ul class="discover">
+            <li>Coming soon titles</li>
+            <li>Sample chapters</li>
+            
+
+          </ul>
+        </div>
+        <div class="connect col-lg-3">
+          <h5 style="margin-left: 30px;">Connect</h5>
+          <ul class="connect">
+            <li>Twitter</li>
+            <li>Facebook</li>
+           
+
+          </ul>
+        </div>
+        <div class="subscribe col-lg-3">
+          <h5 style="margin-left: 30px;">Subscribe</h5>
+          <ul subscribe>
+            <li>Newsletters & offers</li>
+            <li>Deal of the Week RSS</li>
+            
+
+          </ul>
+        </div>
+
+      </div>
+        </div>
+    </div>
+   
+    <?php include 'footer.php';?>
