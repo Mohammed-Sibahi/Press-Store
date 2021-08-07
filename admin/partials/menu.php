9f3d6c3f8ddb2ea7eeb01775c8affb4d7ffb49ea
Mohammed Sibahi <sibahi@live.com>
2021-08-08 00:29:22 +0300

    style the manage-category page

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -17,8 +17,8 @@
                 <li><a href="index.php">Home</a></li>
                 <li><a href="manage-admin.php">Admin</a></li>
                 <li><a href="manage-category.php">Category</a></li>
-                <li><a href="">Books</a></li>
-                <li><a href="">Order</a></li>
+                <li><a href="manage-books.php">Books</a></li>
+                <li><a href="manage-order.php">Order</a></li>
             </ul>
         </div>
 
